abe90004827295ea43bd014f97a2fd96c6fc62ce
cousinroy <89231038@student.upr.si>
2025-08-13 20:56:56 +0200

    added difficulty, fixed loops intersection, added apocalyptic ambient back and some overall design improvements

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -8,11 +8,13 @@ public class GameOverManager : MonoBehaviour
     public TextMeshProUGUI finalTimeText;
     public TextMeshProUGUI gameOverText;
     public GameObject restartButton;
+    public GameObject backToMenuButton;
 
     public void Start()
     {
         gameOverText.gameObject.SetActive(false);
         restartButton.SetActive(false);
+        backToMenuButton.SetActive(false);
     }
 
     public void ShowGameOverScreen(float finalTime)
@@ -26,11 +28,18 @@ public class GameOverManager : MonoBehaviour
         gameOverText.text = "GAME OVER";
         gameOverText.gameObject.SetActive(true);
         restartButton.SetActive(true);
+        backToMenuButton.SetActive(true);
     }
-    
+
     public void RestartGame()
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
     }
 }
